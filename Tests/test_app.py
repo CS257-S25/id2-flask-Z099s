6ffe9b6bcab3b62d0cd9871b7fb52eae2df9e526
“Andrew <andrewgong2005@outlook.com>
2025-05-12 00:47:56 -0500

    revised test files

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -29,13 +29,13 @@ class FlaskAppTest(unittest.TestCase):
 
     def test_sell_arrests_valid(self):
         """Get /<int>/<int> should return the number of people arrested for selling drugs."""
-        response = self.client.get('/1/10')
+        response = self.client.get('sellArrests/1/10')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'566 people', response.data)
 
     def test_sell_arrests_invalid(self):
         """Get /<non-int>/<non-int> should return the invalid-input message."""
-        response = self.client.get('/a/b')
+        response = self.client.get('sellArrests/a/b')
         self.assertEqual(response.status_code, 200)
         self.assertIn(
             b'Invalid input. Please provide valid integers for lower and upper bounds.',
@@ -44,7 +44,7 @@ class FlaskAppTest(unittest.TestCase):
 
     def test_sell_arrests_invalid_int(self):
         """Get /<int>/<non-int> should return the invalid-input message."""
-        response = self.client.get('/1/b')
+        response = self.client.get('sellArrests/1/b')
         self.assertEqual(response.status_code, 200)
         self.assertIn(
             b'Invalid input. Please provide valid integers for lower and upper bounds.',
